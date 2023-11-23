7762d3daf7113e0986ee63f6730019459e3eee78
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-22 21:34:03 -0600

    Termino de primera parte

---
+++ src/main/java/com/mycompany/practica7/Controladora.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica7;
+
+import java.util.Scanner;
+import java.io.*;
+/**
+ *
+ * @author nenod
+ */
+public class Controladora {
+public static void main (String args[]){
+try{
+WebMaker generador = new WebMaker();
+PrintWriter escritor = new PrintWriter("index.html");
+Scanner lector = new Scanner(new FileReader("form.txt"));
+escritor.println(generador.imprimirEncabezado());
+escritor.println(generador.imprimirTexto("Una Gwen chikita trabajando"));
+escritor.println(generador.insertarImagen("trabajando.jpg"));
+while(lector.hasNext()) {
+escritor.println(lector.nextLine());
+}
+escritor.println(generador.imprimirCierre());
+lector.close();
+escritor.close();
+}
+catch (FileNotFoundException e){
+}
+}
+}
