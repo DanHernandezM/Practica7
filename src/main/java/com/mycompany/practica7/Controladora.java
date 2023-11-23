e217a2b2788968c1a59b2dd482f89ffe4d4f3ebd
DanHernandezM <148841520+DanHernandezM@users.noreply.github.com>
2023-11-22 21:59:50 -0600

    Paso 19

--- src/main/java/com/mycompany/practica7/Controladora.java
+++ src/main/java/com/mycompany/practica7/Controladora.java
@@ -6,13 +6,15 @@ package com.mycompany.practica7;
 
 import java.util.Scanner;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author nenod
  */
 public class Controladora {
 public static void main (String args[]){
-try{
+/*try{
 WebMaker generador = new WebMaker();
 PrintWriter escritor = new PrintWriter("index.html");
 Scanner lector = new Scanner(new FileReader("form.txt"));
@@ -27,6 +29,24 @@ lector.close();
 escritor.close();
 }
 catch (FileNotFoundException e){
+}*/
+try{
+DataOutputStream escritor = new DataOutputStream (new FileOutputStream("hola.bin"));
+
+
+escritor.writeInt(33465);
+escritor.writeChar('a');
+escritor.writeChar('z');
+escritor.writeShort(10);
+escritor.close();
+DataInputStream lector = new DataInputStream (new FileInputStream("hola.bin"));
+System.out.println(lector.readInt());
+System.out.println(lector.readChar());
+System.out.println(lector.readChar());
+System.out.println(lector.readShort());
+escritor.close();
 }
+catch (FileNotFoundException e){} 
+catch (IOException ex) {}
 }
 }
